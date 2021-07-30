be13398899336838b47ebb7c8137cdfd8ba2ea06
Ivan <dis010208@mail.ru>
2021-07-30 16:12:19 +0300

    поворот как положено + PAINT(рисование)

--- main.py
+++ main.py
@@ -3,23 +3,47 @@ from tkinter import *
 
 def game_update():
     forward()
-    root.after(100, game_update)
+    paint()
+    root.after(1000, game_update)
+
+
+def paint():
+    c.delete("all")
+    for link in snake:
+        c.create_rectangle(link[0] * 100, link[1] * 100, link[0] * 100 + 100, link[1] * 100 + 100, fill='yellow',
+                           outline='brown', width=3, activedash=(5, 4))
 
 
 def forward():
-    c.move(snake,10,0 )
+    global snake
+    """[6 5] [5 5] [4 5]"""
+    """1 - left"""
+    """2 - right"""
+    """3 - up"""
+    """4 - down"""
+    if direction == 1:
+        x = snake[0][0] - 1
+        y = snake[0][1]
+    if direction == 2:
+        x = snake[0][0] + 1
+        y = snake[0][1]
+    if direction == 3:
+        x = snake[0][0]
+        y = snake[0][1] - 1
+    if direction == 4:
+        x = snake[0][0]
+        y = snake[0][1] + 1
+    target = [x, y]
+    snake = [target] + snake[:-1]
 
 
 root = Tk()
 c = Canvas(root, width=1000, height=1000, bg='light green')
 c.pack()
-direction = 0
+direction = 2
 x = 100
 y = 100
-
-snake = c.create_rectangle(x, y, x + 200, y + 100 , fill='yellow', outline='brown', width=3, activedash=(5, 4))
+snake = [[5, 5], [4, 5], [4, 6],[4, 7],[4,8]]
 eat = c.create_oval(520, 120, 580, 180, fill='red', outline='green', width=3, activedash=(5, 4))
-
-
 game_update()
 root.mainloop()
