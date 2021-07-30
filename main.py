30460b44f790cb4f0f099621eb91ccae922ea90a
Ivan <dis010208@mail.ru>
2021-07-30 16:28:42 +0300

    + управление

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from tkinter import *
 def game_update():
     forward()
     paint()
-    root.after(1000, game_update)
+    root.after(500, game_update)
 
 
 def paint():
@@ -37,13 +37,41 @@ def forward():
     snake = [target] + snake[:-1]
 
 
+def up(event):
+    global direction
+    direction = 3
+
+
+def left(event):
+    global direction
+    direction = 1
+
+
+def right(event):
+    global direction
+    direction = 2
+
+
+def down(event):
+    global direction
+    direction = 4
+
+
 root = Tk()
+root.bind('<Up>', up)
+root.bind('<Down>', down)
+root.bind('<Left>', left)
+root.bind('<Right>', right)
+root.bind('<w>', up)
+root.bind('<s>', down)
+root.bind('<d>', right)
+root.bind('<a>', left)
 c = Canvas(root, width=1000, height=1000, bg='light green')
 c.pack()
 direction = 2
 x = 100
 y = 100
-snake = [[5, 5], [4, 5], [4, 6],[4, 7],[4,8]]
+snake = [[1, 1], [2, 1], [3, 1], [4, 1], [5, 1]]
 eat = c.create_oval(520, 120, 580, 180, fill='red', outline='green', width=3, activedash=(5, 4))
 game_update()
 root.mainloop()
